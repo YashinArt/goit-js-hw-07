6038c487e208c71c81dd72c73eb6c65cb0df1003
YashinArt <93650761+YashinArt@users.noreply.github.com>
2022-05-20 11:00:32 +0300

    1 задание готово

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryConteiner = document.querySelector('.gallery');
+const cardsGallery = createImgCards(galleryItems);
+galleryConteiner.insertAdjacentHTML('beforeend',cardsGallery );
+
+function createImgCards (cards){
+    return cards.map(({ preview,original,description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    
+}).join('');
+};
+
 console.log(galleryItems);
